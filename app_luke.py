089a12ae465fbe0f67005d2967acd5328888f14c
lucasjfsmith <lucasjfsmith@gmail.com>
2023-06-28 11:40:02 -0700

    app completed with filters

--- app_luke.py
+++ app_luke.py
@@ -29,29 +29,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/api/venues")
-def venue():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/venues")
+# def venue():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of all venues"""
-    # Query all venues
-    results = session.query(Venue.venue_id, Venue.venue_name, Venue.venue_state, Venue.venue_address, Venue.venue_lat, Venue.venue_lon).all()
+#     """Return a list of all venues"""
+#     # Query all venues
+#     results = session.query(Venue.venue_id, Venue.venue_name, Venue.venue_state, Venue.venue_address, Venue.venue_lat, Venue.venue_lon).all()
 
-    session.close()
+#     session.close()
 
-    all_venues = []
-    for result in results:
-        venue_dict = {}
-        venue_dict["venue_id"] = result[0]
-        venue_dict["venue_name"] = result[1]
-        venue_dict["venue_state"] = result[2]
-        venue_dict["venue_address"] = result[3]
-        venue_dict["venue_lat"] = float(result[4])
-        venue_dict["venue_lon"] = float(result[5])
-        all_venues.append(venue_dict)
+#     all_venues = []
+#     for result in results:
+#         venue_dict = {}
+#         venue_dict["venue_id"] = result[0]
+#         venue_dict["venue_name"] = result[1]
+#         venue_dict["venue_state"] = result[2]
+#         venue_dict["venue_address"] = result[3]
+#         venue_dict["venue_lat"] = float(result[4])
+#         venue_dict["venue_lon"] = float(result[5])
+#         all_venues.append(venue_dict)
 
-    return jsonify(all_venues)
+#     return jsonify(all_venues)
 
 
 @app.route("/api/teams")
@@ -70,7 +70,7 @@ def team():
     all_teams = []
     for result in results:
         team_dict = {}
-        team_dict["team_id"] = result[0]
+        team_dict["team"] = result[0]
         team_dict["venue_id"] = result[1]
         team_dict["league"] = result[2]
         team_dict["venue_id"] = result[3]
--- static/js/app_luke.js
+++ static/js/app_luke.js
@@ -1,21 +1,53 @@
-// Marker lists for each league
-let nflMarkers = [];
-let nbaMarkers = [];
-let mlbMarkers = [];
-let nhlMarkers = [];
-let mlsMarkers = [];
-
-d3.json("/api/teams").then(teams => {
-
-    console.log(teams);
-});
-
-// for (let i = 0; i < locations.length; i++)
-
-
 let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+let map = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
 
+streetmap.addTo(map);
 
+let layers = {
+    NBA: new L.LayerGroup(),
+    NFL: new L.LayerGroup(),
+    MLB: new L.LayerGroup(),
+    NHL: new L.LayerGroup(),
+    MLS: new L.LayerGroup()
+  };
+
+let overlayMaps = {
+    "NBA": layers.NBA,
+    "NFL": layers.NFL,
+    "MLB": layers.MLB,
+    "NHL": layers.NHL,
+    "MLS": layers.MLS
+  };
+
+L.control.layers(null, overlayMaps).addTo(map);
+
+let colors = {
+    NBA: "orange",
+    NFL: "blue",
+    MLB: "red",
+    NHL: "white",
+    MLS: "green"
+}
+
+d3.json("/api/teams").then(teams => {
+
+    for (let i = 0; i < teams.length; i++) {
+
+        let latlng = [teams[i].venue_lat, teams[i].venue_lon]
+
+        let marker = L.marker(latlng);
+
+        marker.bindPopup(`<h2>${teams[i].venue_name}</h2><br>` +
+                         `<h3>${teams[i].league} Team: ${teams[i].team}</h3><hr>` +
+                         `<p>${teams[i].venue_address}</p>`
+        );
+
+        marker.addTo(layers[teams[i].league]);
+    };
+});
