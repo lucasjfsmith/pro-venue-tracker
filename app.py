747a26836494c4ce7001c244186a025f13a87263
lucasjfsmith <lucasjfsmith@gmail.com>
2023-10-24 13:37:20 -0500

    api test 3

--- app.py
+++ app.py
@@ -139,6 +139,34 @@ def test():
         else:
             venues_dict[venue_name].append(team_dict)
 
+    today = dt.date.today()
+
+    for venue in venues_dict:
+        for team in venues_dict[venue]:
+            # if team["league"] != "NBA":
+            home_id = team["team_id"]
+
+            next_event = session.query(Event.timestamp, Event.away_id).\
+                                        filter(Event.home_id == home_id).\
+                                        filter(Event.timestamp > today).\
+                                        order_by(Event.timestamp).limit(1).all()
+            
+            print(venues_dict[venue])
+
+            event_time = next_event[0][0].strftime("%Y-%m-%d %I:%M %p") + " EST"
+
+
+            team["next_event"] = [event_time]
+            away_id = next_event[0][1]
+
+            away_team = session.query(Team.team).filter(Team.team_id == away_id)[0][0]
+            team["next_event"].append(away_team)
+
+            # else:
+            #     team["next_event"] = "NBA Schedule not yet released."
+
+    session.close()
+
     return jsonify(venues_dict)
     
 if __name__ == '__main__':
