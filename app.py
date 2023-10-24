3786ec0682ea5235025d7fd87e92ef44cf922381
lucasjfsmith <lucasjfsmith@gmail.com>
2023-10-24 15:55:44 -0500

    api fix  7

--- app.py
+++ app.py
@@ -79,25 +79,25 @@ def venue():
 
     for venue in venues_dict:
         for team in venues_dict[venue]:
-            if team["league"] != "MLS":
-                home_id = team["team_id"]
 
-                next_event = session.query(Event.timestamp, Event.away_id).\
-                                            filter(Event.home_id == home_id).\
-                                            filter(Event.timestamp > today).\
-                                            order_by(Event.timestamp).limit(1).all()
+            home_id = team["team_id"]
 
+            next_event = session.query(Event.timestamp, Event.away_id).\
+                                        filter(Event.home_id == home_id).\
+                                        filter(Event.timestamp > today).\
+                                        order_by(Event.timestamp).limit(1).all()
+            
+            try:
                 event_time = next_event[0][0].strftime("%Y-%m-%d %I:%M %p") + " EST"
-
 
                 team["next_event"] = [event_time]
                 away_id = next_event[0][1]
 
                 away_team = session.query(Team.team).filter(Team.team_id == away_id)[0][0]
                 team["next_event"].append(away_team)
-
-            else:
-                team["next_event"] = "Season has concluded."
+            
+            except:
+                team["next_event"] = 'Regular season has ended.'
 
     session.close()
 
@@ -116,8 +116,6 @@ def test():
                             Venue.venue_lat, Venue.venue_lon).\
                                 filter(Team.league_id == League.league_id).\
                                 filter(Team.venue_id == Venue.venue_id).all()
-    
-    print(len(results))
 
     venues_dict = {}
 
@@ -166,7 +164,7 @@ def test():
                 team_error.append(team['team'])
                 team["next_event"] = 'Regular season has ended.'
 
-    print(team_error)
+    print(len(team_error))
 
     session.close()
 
